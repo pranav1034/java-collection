e675422e1233b94d96ad218d39976fef14301b67
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-28 10:50:41 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/listinterface/ReverseList.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.listinterface;
+import java.util.*;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        System.out.println("Enter the elements for the list:");
+
+
+    }
+}
