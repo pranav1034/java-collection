bd3ac64bc1efb63024843d953eaf59d1dc7eebaf
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-28 13:01:04 +0530

    [Pranav] ADD remove-duplicates-from-list-while-preserving-order

--- src/main/java/com/bridgelabz/listinterface/RemoveDuplicates.java
+++ src/main/java/com/bridgelabz/listinterface/RemoveDuplicates.java
@@ -5,12 +5,19 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>(Arrays.asList(3,1,2,2,3,4));
         System.out.println("List before removing duplicates:");
+        System.out.println(list);
+
         HashSet<Integer> set = new HashSet<>();
-        for(int i:list){
-            if(!set.contains(i)){
-                System.out.print(i+" ");
+        for(int i=0;i<list.size();i++){
+            if(!set.contains(list.get(i))){
+                set.add(list.get(i));
             }
-            set.add(i);
+            else {
+                list.remove(i);
+                i--;
+            }
         }
+        System.out.println("List after removing duplicates:");
+        System.out.println(list);
     }
 }
